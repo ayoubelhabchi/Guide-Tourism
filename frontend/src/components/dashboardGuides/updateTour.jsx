d0ad9d6096c716df530df9fe35ecb7d214fc88f2
ayoubelhabchi <ayoub.elhabchi.2000@gmail.com>
2024-08-05 20:37:09 +0100

    styling create tour dahsboard

--- frontend/src/components/dashboardGuides/updateTour.jsx
+++ frontend/src/components/dashboardGuides/updateTour.jsx
@@ -100,7 +100,7 @@ function UpdateTour() {
       <div className='text-3xl font-semibold text-primary mb-2'>Update Tour</div>
       <hr className='my-4 border-slate-400' />
 
-      <form onSubmit={handleSubmit} className='grid lg:grid-cols-2 items-center lg:gap-x-16'>
+      <form onSubmit={handleSubmit} className='grid lg:grid-cols-2 grid-cols-1 items-center lg:gap-x-16'>
         <div className='space-y-2'>
           <div> 
             <label htmlFor='title' className='block text-sm font-medium text-gray-700 mb-1'>Title</label>
