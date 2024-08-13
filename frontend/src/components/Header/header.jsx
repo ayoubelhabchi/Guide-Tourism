a883471563abadb46fcfeac05142e7ae5ee1477a
ayoubelhabchi <ayoub.elhabchi.2000@gmail.com>
2024-08-13 15:09:00 +0100

    Icons improvements

--- frontend/src/components/Header/header.jsx
+++ frontend/src/components/Header/header.jsx
@@ -65,8 +65,8 @@ const Header = ({ handleProfile }) => {
         <div className="container mx-auto lg:px-3">
           <div className="lg:w-full w-11/12 mx-auto h-full flex justify-between items-center">
             <img
-              src={location.pathname === '/orders' ? "/logoDark.png" : (isScrolled ? "/logoLight.png" : "/logoDark.png")}
-              className="lg:h-40 h-28 rounded[103] objectcover -ml8"
+              src={location.pathname === '/orders' ? "/logoDark.png" : (isScrolled ? "/LogoLight.png" : "/LogoDark.png")}
+              className="lg:h-44 h-28 rounded[103] objectcover -ml8"
               alt="Logo"
             />
             {isLoggedIn && ( 
